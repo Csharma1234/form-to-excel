67fbacd75f9c38ad588d8e9c92a57d3231a98847
Utsab Haldar <utsabhaldar88888@gmail.com>
2024-08-25 20:25:01 +0530

    vercel

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render('app');
 });
 
 app.post('/submit', (req, res) => {
