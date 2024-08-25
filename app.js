62cc6ff300c2b8e1b4d51800933e8f08c396a9ce
Utsab Haldar <utsabhaldar88888@gmail.com>
2024-08-25 23:22:21 +0530

    update

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const QRCode = require('qrcode');
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
-app.use(express.static('views'));
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
   res.render('index');
