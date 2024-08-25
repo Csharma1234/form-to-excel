6237ae4c3b24f37a5ec710074830b0d8f3e92016
Utsab Haldar <utsabhaldar88888@gmail.com>
2024-08-25 21:49:37 +0530

    vercel

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const QRCode = require('qrcode');
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
-app.use(express.static('public'));
+app.use(express.static('views'));
 
 app.get('/', (req, res) => {
   res.render('index');
